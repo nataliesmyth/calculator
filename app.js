cd56f45ee74c848e41aa3a60cb23fe0e12d9a92d
Nataliesmyth <nataliesmyth11@gmail.com>
2022-12-16 17:47:04 -0500

    clicked number buttons show up in display

--- app.js
+++ app.js
@@ -1,3 +1,41 @@
+// second step, but clearer when class is at the top
+// What's the best way to create the calculator functionality?
+// How are we going to store the imformation given when the user clicks a specific button?
+// Classes
+
+class Calculator {
+    constructor(previousOperandTextEl, currentOperandTextEl) {
+        this.previousOperandTextEl = previousOperandTextEl
+        this.currentOperandTextEl = currentOperandTextEl
+        this.clear()
+    }
+    
+    clear() {
+        this.currentOperand = '';
+        this.previousOperand = '';
+        this.operation = undefined;
+    }
+    
+    delete() {
+        
+    }
+    
+    appendNumber(number) {
+        this.currentOperand = number
+    }
+    
+    chooseOperation(operation) {
+        
+    }
+    
+    compute() {
+        
+    }
+    
+    updateDisplay() {
+        this.currentOperandTextEl.innerText = this.currentOperand
+    }
+}
 // first step: select all of our buttons
 // we will use data attributes to keep styling classes and JS classes separate
 
@@ -8,3 +46,15 @@ const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextEl = document.querySelector('[data-previous-operand]')
 const currentOperandTextEl = document.querySelector('[data-current-operand]')
+
+// step 3: create a calculator
+
+const calculator = new Calculator(previousOperandTextEl, currentOperandTextEl);
+
+// select number buttons
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();
+    })
+})
